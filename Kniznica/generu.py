27acc67a2e9015559951da74f8ec06bedd60eed7
POL <patrik.hrobarik@student.ssnd.sk>
2025-12-04 13:07:06 +0100

    prog13.1

--- Kniznica/generu.py
+++ Kniznica/generu.py
@@ -2,6 +2,6 @@ from random import randint
 from graf import graf 
 hodnoty = []
 for u in range (100):
-    hodnoty.append(randint(1,6))
+    hodnoty.append(randint(1,20))
 hodnoty.sort()
 graf(hodnoty)
